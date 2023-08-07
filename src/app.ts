6e142b377e638dbe1c89b937feda6830836aea89
Anton Chepko <choptonys@gmail.com>
2023-08-07 22:51:31 +0200

    3. mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import express, { Request, Response } from "express";
+import * as mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/users", (req: Request, res: Response) => {
+app.get("/cars", (req: Request, res: Response) => {
   // res.status(200).json(users);
-  console.log("get all users");
+  console.log("get all cars");
 });
 
 const PORT = 5001;
 
 app.listen(PORT, () => {
+  mongoose.connect(
+    "mongodb+srv://antonche:antonche@jscomplex-back-end-test.agf2szy.mongodb.net/"
+  );
   console.log(`Server has started on PORT ${PORT}`);
 });
